d58104c9f8bc7d5563504dfc23875f0fa6ded683
Ramasubramanian Chandrasekar <chandrasekar.r@husky.neu.edu>
2016-05-28 23:19:03 -0400

    Added register controller

--- public/assignment/views/user/register.controller.client.js
+++ public/assignment/views/user/register.controller.client.js
@@ -3,12 +3,30 @@
         .module("WebAppMaker")
         .controller("RegisterController", RegisterController);
 
-    function RegisterController($location, RegisterService) {
+    function RegisterController($location, UserService) {
         var vm = this;
-        vm.registerUser = registerUser
+        vm.registerUser = registerUser;
 
-        function registerUser (username, password) {
-            
+        function registerUser (username, password, verifypassword) {
+            if(username === "" || password === ""){
+                if(password === verifypassword) {
+                    var newUser = {
+                        username: username,
+                        password: password
+                    }
+                    var user = UserService.createUser(newUser);
+                    if(user) {
+                        $location.url("/user/"+user._id);
+                    } else {
+                        vm.error = "User not created";
+                    }
+                } else {
+                    vm.error = "Passwords do not match";
+                }
+            }
+            else {
+                vm.error = "Username/password cannot be blank"
+            }
         }
     }
 })();
